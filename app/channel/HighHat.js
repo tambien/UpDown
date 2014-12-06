6707fd821ee644c9cbf7c26e9eb7be19a14656dc
Yotam Mann <yotammann@gmail.com>
2014-12-06 17:49:32 -0500

    working on HH sounds

--- app/channel/HighHat.js
+++ app/channel/HighHat.js
@@ -3,31 +3,31 @@ define(["Tone/instrument/NoiseSynth", "controller/Mediator",
  "Tone/component/Filter", "Tone/component/PanVol", "interface/GUI"], 
 function(NoiseSynth, Mediator, Preset, Conductor, Master, Transport, Filter, PanVol, GUI){
 
-	var synth = new NoiseSynth();
+	var synth = new NoiseSynth({
+		"envelope" : {
+			"sustain" : 0.0,
+		},
+		"filterEnvelope" : {
+			"sustain" : 0,
+		}
+	});
 
 	GUI.addTone("High Hat", "synth", synth, {
 		"noise" : {
 			"type" : "white"
 		},
 		"filter" : {
-			"Q" : 6,
 			"type" : "highpass",
 			"rolloff" : -12
 		},
 		"envelope" : {
 			"attack" : 0.005,
 			"decay" : 0.1,
-			"sustain" : 0.0,
-			"release" : 0.3,
 			"exponent" : 2
 		},
 		"filterEnvelope" : {
-			"attack" : 0.006,
-			"decay" : 0.02,
-			"sustain" : 0,
-			"release" : 0.5,
-			"min" : 5000,
-			"max" : 10000,
+			"min": 13000,
+			"max": 3900,
 			"exponent" : 2
 		}
 	});
@@ -47,7 +47,7 @@ function(NoiseSynth, Mediator, Preset, Conductor, Master, Transport, Filter, Pan
 	filt.toMaster();
 
 	//reverb
-	var revAmount = synth.send("reverb", synth.dbToGain(-50));
+	var revAmount = filt.send("reverb", synth.dbToGain(-50));
 
 	GUI.addSlider("High Hat", "reverb", -50, -100, 0, function(val){
 		revAmount.gain.value = filt.dbToGain(val);
@@ -65,7 +65,7 @@ function(NoiseSynth, Mediator, Preset, Conductor, Master, Transport, Filter, Pan
 	return {
 		triggerAttackRelease : function(duration, time){
 			if (hasChanged){
-				hasChanged = false;
+				// hasChanged = false;
 				synth.set(Preset.get(position));
 			}
 			synth.triggerAttack(time);
--- app/preset/HighHatSound.js
+++ app/preset/HighHatSound.js
@@ -1,40 +1,37 @@
 define(["preset/Interpolator", "interface/PresetGUI"], function(Interpolator, GUI){
 
-	var HHSmoothPreset = [
+	var HHSmoothPreset = 
+	[
 		{
 			"filter": {
-				"Q": 5
+				"Q": 7.1
 			},
 			"envelope": {
-				"attack": 0.002,
-				"decay": 0.01
+				"attack": 0.0007,
+				"decay": 0.009
 			},
 			"filterEnvelope": {
-				"attack": 0.006,
-				"decay": 0.005,
-				"min": 4000,
-				"max": 200
+				"attack": 0.0033,
+				"decay": 0.0119
 			}
 		},
 		{
 			"filter": {
-				"Q": 20
+				"Q": 10
 			},
 			"envelope": {
-				"attack": 0.001,
-				"decay": 0.03
+				"attack": 0.02,
+				"decay": 0.065
 			},
 			"filterEnvelope": {
-				"attack": 0.005,
-				"decay": 0.08,
-				"min": 13000,
-				"max": 1000
+				"attack": 0.0002,
+				"decay": 0.12
 			}
 		}
 	];
 
 	GUI.addPreset("High Hat Smooth", HHSmoothPreset);
-	var HighHatSmooth = new Interpolator(HHSmoothPreset, "smooth", 2);
+	var HighHatSmooth = new Interpolator(HHSmoothPreset, "smooth");
 
 	return HighHatSmooth;
 });
